1a24cb14fd69dd0b3340365b8996adc35b87da17
Gkoski <gabrieljoukoskii@gmail.com>
2026-05-28 00:29:04 -0300

    criando arquivos e pacotes necessários

---
+++ gestao-consultas/src/test/java/com/project/api/gestaoconsultas/GestaoConsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.api.gestaoconsultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoConsultasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
