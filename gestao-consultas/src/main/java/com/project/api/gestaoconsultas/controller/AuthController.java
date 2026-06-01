f77519c5c8a69f299186395b4169f97524ed4550
Gkoski <gabrieljoukoskii@gmail.com>
2026-05-31 23:50:32 -0300

    arrumando imports

--- gestao-consultas/src/main/java/com/project/api/gestaoconsultas/controller/AuthController.java
+++ gestao-consultas/src/main/java/com/project/api/gestaoconsultas/controller/AuthController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
